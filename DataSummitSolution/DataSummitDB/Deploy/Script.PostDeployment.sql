e2a88506f647b8fadbface4ea47f0dec4e6c336f
Thomas James <tom.james@data-summit.co.uk>
2020-04-13 17:56:42 +0100

    SQL Post Deploy Script
    SQL post deploy script for seeded data

---
+++ DataSummitSolution/DataSummitDB/Deploy/Script.PostDeployment.sql
@@ -0,0 +1,33 @@
+﻿/*
+Post-Deployment Script Template							
+--------------------------------------------------------------------------------------
+To use this script, right mouste button > Execution Settings > SQLCMD Mode
+
+Note: This is a seeding only data set. Only to run on empty databases
+--------------------------------------------------------------------------------------
+*/
+
+:r ..\dbo\Data\AspNetRoles.sql
+:r ..\dbo\Data\Companies.sql
+:r ..\dbo\Data\Genders.sql
+:r ..\dbo\Data\UserTypes.sql
+:r ..\dbo\Data\AspNetUsers.sql
+:r ..\dbo\Data\UserInfoTypes.sql
+:r ..\dbo\Data\UserInfo.sql
+:r ..\dbo\Data\BlockPositions.sql
+:r ..\dbo\Data\PaperSizes.sql
+:r ..\dbo\Data\StandardAttributes.sql
+:r ..\dbo\Data\AzureCompanyResourceUrls.sql
+:r ..\dbo\Data\Projects.sql
+:r ..\dbo\Data\Countries.sql
+:r ..\dbo\Data\Currencies.sql
+:r ..\dbo\Data\Addresses.sql
+:r ..\dbo\Data\PaperOrientations.sql
+:r ..\dbo\Data\Drawings.sql
+:r ..\dbo\Data\ImageGrids.sql
+:r ..\dbo\Data\Sentences.sql
+:r ..\dbo\Data\Tasks.sql
+:r ..\dbo\Data\GoogleLanguages.sql
+:r ..\dbo\Data\ProfileVersions.sql
+:r ..\dbo\Data\ProfileAttributes.sql
+:r ..\dbo\Data\Properties.sql
