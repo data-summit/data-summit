7c2582ef20ac2a3ec1da62006c5cb2a4d08ddb24
Rob B <robert.blankson@hudl.com>
2021-04-07 09:57:40 +0100

    Removing unused tables

--- DataSummitSolution/DataSummitDB/dbo/Tables/EmployeeTerritories.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[EmployeeTerritories] (
-    [EmployeeId]  INT           NOT NULL,
-    [TerritoryId] NVARCHAR (20) NOT NULL,
-    [CreatedDate] DATETIME      NULL,
-    [UserId]      BIGINT        NULL,
-    CONSTRAINT [PK_EmployeeTerritories] PRIMARY KEY CLUSTERED ([EmployeeId] ASC, [TerritoryId] ASC)
-);
-
--- DataSummitSolution/DataSummitDB/dbo/Tables/UserInfoTypes.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[UserInfoTypes] (
-    [UserInfoTypeId] TINYINT       IDENTITY (1, 1) NOT NULL,
-    [Type]              NVARCHAR (63) NOT NULL,
-    [CreatedDate]       DATETIME      NULL,
-    [UserId]            BIGINT        NULL,
-    CONSTRAINT [PK_UserInfoTypes] PRIMARY KEY CLUSTERED ([UserInfoTypeId] ASC)
-);
-
