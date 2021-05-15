f16f1069b21b13f3f7441a7a56a1c116797ba6f5
Tom James <52748031+TomJamesAtDataSummit@users.noreply.github.com>
2021-05-15 13:55:46 +0100

    Azure Function ObjectDetection
    Azure Function ObjectDetection throw error due incorrect prediction class being called. Line 60/55

--- DataSummitSolution/AzureFunctions/MachineLearning/ObjectDetection.cs
+++ DataSummitSolution/AzureFunctions/MachineLearning/ObjectDetection.cs
@@ -21,7 +21,8 @@ namespace AzureFunctions.MachineLearning
 {
     public static class ObjectDetection
     {
-        private static readonly string endPoint = @"https://documentlayout.cognitiveservices.azure.com";
+        private static readonly string ENDPOINT = @"https://documentlayout.cognitiveservices.azure.com";
+
         [FunctionName("ObjectDetection")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
@@ -35,71 +36,69 @@ namespace AzureFunctions.MachineLearning
                 CustomVision cvML = (CustomVision)data;
 
                 //Verify body content
-                if (cvML.BlobUrl == "") return new BadRequestObjectResult("Illegal input: blob url required.");
-                if (cvML.MLUrl == "") return new BadRequestObjectResult("Illegal input: Ml end-point url required.");
-                if (cvML.TrainingKey == "") return new BadRequestObjectResult("Illegal input: No training key");
-                if (cvML.PredictionKey == "") return new BadRequestObjectResult("Illegal input: No prediction key");
-                if (cvML.MLProjectName == "") return new BadRequestObjectResult("Illegal input: ML project name required");
+                if (string.IsNullOrEmpty(cvML.BlobUrl)) return new BadRequestObjectResult("Illegal input: blob url required.");
+                if (string.IsNullOrEmpty(cvML.MLUrl)) return new BadRequestObjectResult("Illegal input: Ml end-point url required.");
+                if (string.IsNullOrEmpty(cvML.TrainingKey)) return new BadRequestObjectResult("Illegal input: No training key");
+                if (string.IsNullOrEmpty(cvML.PredictionKey)) return new BadRequestObjectResult("Illegal input: No prediction key");
+                if (string.IsNullOrEmpty(cvML.MLProjectName)) return new BadRequestObjectResult("Illegal input: ML project name required");
 
                 // Create a training endpoint, passing in the obtained training key
-                CustomVisionTrainingClient trainingApi = new CustomVisionTrainingClient(
+                var trainingApi = new CustomVisionTrainingClient(
                     new Microsoft.Azure.CognitiveServices.Vision.CustomVision.Training.ApiKeyServiceClientCredentials(cvML.TrainingKey))
-                { Endpoint = endPoint };
+                { Endpoint = ENDPOINT };
 
                 var projects = await trainingApi.GetProjectsAsync();
                 if (projects != null && projects.Count(p => p.Name == cvML.MLProjectName) > 0)
                 {
-                    var project = projects.First(p => p.Name == cvML.MLProjectName);
+                    var project = projects.Single(p => p.Name == cvML.MLProjectName);
 
-                    List<ClassificationPrediction> preds = new List<ClassificationPrediction>();
-                    if (project != null)
+                    var preds = new List<ObjectDetectionPrediction>();
+
+                    // Create a prediction endpoint, passing in the obtained prediction key
+                    var predictionApi = new CustomVisionPredictionClient(
+                        new Microsoft.Azure.CognitiveServices.Vision.CustomVision.Prediction.ApiKeyServiceClientCredentials(cvML.PredictionKey));
+                    predictionApi.Endpoint = ENDPOINT;
+
+                    var imgUrl = new ImageUrl(cvML.BlobUrl)
                     {
-                        // Create a prediction endpoint, passing in the obtained prediction key
-                        CustomVisionPredictionClient predictionApi = new CustomVisionPredictionClient(
-                            new Microsoft.Azure.CognitiveServices.Vision.CustomVision.Prediction.ApiKeyServiceClientCredentials(cvML.PredictionKey));
-                        predictionApi.Endpoint = endPoint;
+                        Url = cvML.BlobUrl
+                    };
 
-                        var imgUrl = new ImageUrl(cvML.BlobUrl)
+                    var iteration = cvML.GetIteration();
+                    var result = predictionApi.DetectImageUrl(project.Id, iteration, imgUrl);
+
+                    foreach (var c in result.Predictions)
+                    {
+                        if (c.Probability > cvML.MinThreshold)
                         {
-                            Url = cvML.BlobUrl
-                        };
-
-                        var iteration = cvML.GetIteration();
-                        var result = predictionApi.DetectImageUrl(project.Id, iteration, imgUrl);
-
-                        foreach (var c in result.Predictions)
-                        {
-                            if (c.Probability > cvML.MinThreshold)
+                            var pred = new ObjectDetectionPrediction()
                             {
-                                var pred = new ObjectDetectionPrediction()
+                                Probability = c.Probability,
+                                TagId = c.TagId,
+                                TagName = c.TagName,
+                                TagType = c.TagType
+                            };
+                            if (c.BoundingBox != null)
+                            {
+                                pred.BoundingBox = new DataSummitModels.BHoM.Geometry.BoundingBox()
                                 {
-                                    Probability = c.Probability,
-                                    TagId = c.TagId,
-                                    TagName = c.TagName,
-                                    TagType = c.TagType
-                                };
-                                if (c.BoundingBox != null)
-                                {
-                                    pred.BoundingBox = new DataSummitModels.BHoM.Geometry.BoundingBox()
+                                    Max = new DataSummitModels.BHoM.Geometry.Point()
                                     {
-                                        Max = new DataSummitModels.BHoM.Geometry.Point()
-                                        {
-                                            X = c.BoundingBox.Left + c.BoundingBox.Width,
-                                            Y = c.BoundingBox.Top
-                                        },
-                                        Min = new DataSummitModels.BHoM.Geometry.Point()
-                                        {
-                                            X = c.BoundingBox.Left,
-                                            Y = c.BoundingBox.Top - c.BoundingBox.Height
-                                        }
-                                    };
-                                }
-                                preds.Add(pred);
+                                        X = c.BoundingBox.Left + c.BoundingBox.Width,
+                                        Y = c.BoundingBox.Top
+                                    },
+                                    Min = new DataSummitModels.BHoM.Geometry.Point()
+                                    {
+                                        X = c.BoundingBox.Left,
+                                        Y = c.BoundingBox.Top - c.BoundingBox.Height
+                                    }
+                                };
                             }
+                            preds.Add(pred);
                         }
                     }
 
-                    string jsonToReturn = JsonConvert.SerializeObject(preds);
+                    var jsonToReturn = JsonConvert.SerializeObject(preds);
                     return new OkObjectResult(jsonToReturn);
                 }
                 else
