34f22d6f08b999e7c41979d5804884142e41e11a
Rob B <robert.blankson@hudl.com>
2021-05-17 09:46:48 +0100

    Initial commit

---
+++ DataSummitSolution/DataSummitHelper/Dto/DocumentTypeSummaryDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataSummitService.Dto
+{
+    public class DocumentTypeSummaryDto
+    {
+        public string BlobUrl { get; set; }
+        public string DocumentType { get; set; }
+    }
+}
---
+++ DataSummitSolution/DataSummitHelper/Dto/FileUploadSummaryDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataSummitService.Dto
+{
+    public class FileUploadSummaryDto
+    {
+        public string BlobUrl { get; set; }
+        public string FileName { get; set; }
+        public string FileExtension { get; set; }
+    }
+}
