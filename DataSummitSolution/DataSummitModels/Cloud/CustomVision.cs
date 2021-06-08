58f945f1722c672513f19e6b3a61f9384c105719
Rob B <robert.blankson@hudl.com>
2021-06-08 10:42:59 +0100

    Use regex to find Iteration number

--- DataSummitSolution/DataSummitModels/Cloud/CustomVision.cs
+++ DataSummitSolution/DataSummitModels/Cloud/CustomVision.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Text.RegularExpressions;
+
 namespace DataSummitModels.Cloud
 {
     public class CustomVision
@@ -15,9 +17,10 @@ namespace DataSummitModels.Cloud
 
         public string GetIteration()
         {
-            return MLUrl.Substring(
-                        MLUrl.LastIndexOf("/Iteration") + 1,
-                        MLUrl.LastIndexOf("/") - MLUrl.LastIndexOf("/Iteration") - 1);
+            // Return the number from an example string http://test/Iteration/1323/
+            var rgx = new Regex(@"(?<=Iteration\/)(\d+)(?=\/)");
+            var match = rgx.Match(MLUrl);
+            return match.Value;
         }
     }
 }
