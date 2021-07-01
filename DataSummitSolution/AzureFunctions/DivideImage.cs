4e4f71541c6dcf51635f608b3e40459137040370
Rob B <robert.blankson@hudl.com>
2021-07-01 12:25:37 +0100

    Tidy up code
    Still needs a couple of final tweaks and testing

--- DataSummitSolution/AzureFunctions/DivideImage.cs
+++ DataSummitSolution/AzureFunctions/DivideImage.cs
@@ -54,7 +54,7 @@ namespace AzureFunctions
                 var cloudBlockBlob = blobContainerClient.GetBlobClient(OriginalJpeg);
 
                 using var memoryStream = new MemoryStream();
-                var data = await cloudBlockBlob.DownloadToAsync(memoryStream);
+                _ = await cloudBlockBlob.DownloadToAsync(memoryStream);
                 memoryStream.Seek(0, SeekOrigin.Begin);
                 var image = Image.FromStream(memoryStream);
 
@@ -68,8 +68,8 @@ namespace AzureFunctions
                 var jsonBlob = blobContainerClient.GetBlobClient("Split Images Data & Structure.json");
                 var imageUploadJson = JsonConvert.SerializeObject(splitImages, Formatting.Indented);
 
-                using var ms = new MemoryStream(Encoding.UTF8.GetBytes(imageUploadJson));
-                await jsonBlob.UploadAsync(ms);
+                using var imageUploadStream = new MemoryStream(Encoding.UTF8.GetBytes(imageUploadJson));
+                await jsonBlob.UploadAsync(imageUploadStream);
 
                 return new OkObjectResult(imageUploadJson);
             }
@@ -79,71 +79,25 @@ namespace AzureFunctions
             }
         }
 
-        private static async Task<BlobItem> FindOriginalJpeg(BlobContainerClient blobContainerClient, string blobUrl)
-        {
-            // v12
-            var blobsAsPages = blobContainerClient.GetBlobsAsync().AsPages();
-            await foreach (var blobPage in blobsAsPages)
-            {
-                foreach (var blobItem in blobPage.Values)
-                {
-
-                    if (blobItem.Name == OriginalJpeg)
-                    { return blobItem; }
-                }
-            }
-            return null;
-            //// v11
-            //var blobResultSegment = await blobContainerClient.ListBlobsSegmentedAsync(null);
-            //var blobContinuationToken = blobResultSegment.ContinuationToken;
-            //var blobSegmentResults = blobResultSegment.Results;
-            //do
-            //{
-            //    bool resultsContainBlobUrl = blobSegmentResults.Any(b => b.Uri.ToString() == blobUrl);
-            //    if (resultsContainBlobUrl)
-            //    {
-            //        var firstMatchingResult = blobSegmentResults.First(b => b.Uri.ToString() == blobUrl);
-            //        return firstMatchingResult as CloudBlockBlob;
-            //    }
-            //    else
-            //    {
-            //        foreach (var blobItem in blobSegmentResults)
-            //        {
-            //            if (blobItem is CloudBlockBlob)
-            //            {
-            //                CloudBlockBlob cloudBlockBlob = blobItem as CloudBlockBlob;
-
-            //                if (cloudBlockBlob.Name == OriginalJpeg)
-            //                {
-            //                    return cloudBlockBlob;
-            //                }
-            //            }
-            //        }
-
-            //        return null;
-            //    }
-            //} while (blobContinuationToken != null);
-        }
-
-        private static async Task<string> UploadImageSectionToBlob(BlobContainerClient blobContainerClient,
-                                                           ImageSectionDto splitImage)
+        private static async Task<string> UploadImageSectionToBlob(BlobContainerClient blobContainerClient, ImageSectionDto splitImage)
         {
 
             var identifier = "mysignedidentifier";
             var readWritePermission = "rw";
-            var blobSignedIdentifier = new BlobSignedIdentifier()
-            {
-                Id = identifier,
-                AccessPolicy = new BlobAccessPolicy
-                {
-                    StartsOn = DateTimeOffset.UtcNow.AddHours(-1),
-                    ExpiresOn = DateTimeOffset.UtcNow.AddDays(1),
-                    Permissions = readWritePermission
-                }
-            };
 
             var signedIdentifiers = new List<BlobSignedIdentifier>
-            { blobSignedIdentifier };
+            {
+                new BlobSignedIdentifier
+                {
+                    Id = identifier,
+                    AccessPolicy = new BlobAccessPolicy
+                    {
+                        StartsOn = DateTimeOffset.UtcNow.AddHours(-1),
+                        ExpiresOn = DateTimeOffset.UtcNow.AddDays(1),
+                        Permissions = readWritePermission
+                    }
+                }
+            };
             await blobContainerClient.SetAccessPolicyAsync(PublicAccessType.BlobContainer, signedIdentifiers);
 
             // Export image to blockBlob
@@ -163,42 +117,36 @@ namespace AzureFunctions
             using var memStream = new MemoryStream();
             splitImage.Image.Save(memStream, ImageFormat.Jpeg);
             memStream.Seek(0, SeekOrigin.Begin);
-            var blobContainerInfo = await blobContainerClient.UploadBlobAsync(splitImage.Name, memStream);
+            _ = await blobContainerClient.UploadBlobAsync(splitImage.Name, memStream);
+            
             var blobClient = blobContainerClient.GetBlobClient(splitImage.Name);
-
             await blobClient.SetMetadataAsync(blobUploadOptions.Metadata);
             return blobClient.Uri.ToString();
         }
 
-        private static async Task<List<ImageSectionDto>> CreateImageSections(Bitmap bitmap, List<ImagePortion> imagePortions,
-                                                                     BlobContainerClient blobContainerClient)
+        private static async Task<List<ImageSectionDto>> CreateImageSections(Bitmap bitmap, List<ImagePortion> imagePortions, BlobContainerClient blobContainerClient)
         {
             var standardGrid = new List<ImageSectionDto>();
 
             foreach (var imagePortion in imagePortions)
             {
+                var width = imagePortion.AdjustedWith > bitmap.Width ? (bitmap.Width - imagePortion.WidthPixel) : (int)imagePortion.WidthCalculatedSpan;
+                var height = imagePortion.AdjustedHeight > bitmap.Height ? bitmap.Height - imagePortion.HeightPixel : (int)imagePortion.HeightCalculatedSpan;
                 var imageSection = new ImageSectionDto
                 {
                     Name = imagePortion.Name,
                     WidthStart = imagePortion.WidthPixel,
                     HeightStart = imagePortion.HeightPixel,
-                    Width = (int)imagePortion.WidthCalculatedSpan,
-                    Height = (int)imagePortion.HeightCalculatedSpan,
+                    Width = width,
+                    Height = height,
+                    ImageType = imagePortion.Prefix switch
+                    {
+                        'F' => ImageType.Normal,
+                        _ => ImageType.Overlap,
+                    },
+                    Image = bitmap.Clone(new Rectangle(imagePortion.WidthPixel, imagePortion.HeightPixel, width, height), PixelFormat.Format24bppRgb)
                 };
 
-                // Assign type from prefix
-                if (imagePortion.Prefix == 'F')
-                { imageSection.ImageType = ImageType.Normal; }
-                else
-                { imageSection.ImageType = ImageType.Overlap; }
-
-                // Check that final pixel is within range of bitmap dimensions
-                if ((imageSection.WidthStart + imageSection.Width) > bitmap.Width)
-                { imageSection.Width = bitmap.Width - imageSection.WidthStart; }
-                if ((imageSection.HeightStart + imageSection.Height) > bitmap.Height)
-                { imageSection.Height = bitmap.Height - imageSection.HeightStart; }
-
-                imageSection.Image = bitmap.Clone(new Rectangle(imageSection.WidthStart, imageSection.HeightStart, imageSection.Width, imageSection.Height), PixelFormat.Format24bppRgb);
                 standardGrid.Add(imageSection);
                 imageSection.BlobUrl = await UploadImageSectionToBlob(blobContainerClient, imageSection);
             }
@@ -206,105 +154,13 @@ namespace AzureFunctions
             return standardGrid;
         }
 
-        private static async Task<List<ImageSectionDto>> CreateImageSections(int widthMod,
-                                                                     int heightMod,
-                                                                     int widthSpan,
-                                                                     int heightSpan,
-                                                                     int widthAdjust,
-                                                                     int heightAdjust,
-                                                                     Bitmap bitmap,
-                                                                     BlobServiceClient blobServiceClient,
-                                                                     BlobContainerClient blobContainerClient)
-        {
-            var standardGrid = new List<ImageSectionDto>();
-
-            for (int x = 0; x < widthMod; x++)
-            {
-                for (int y = 0; y < heightMod; y++)
-                {
-                    var widthStart = x * widthSpan;
-                    var heightStart = y * heightSpan;
-                    var imageSection = new ImageSectionDto
-                    {
-                        Name = $"F_{x:000}-{y:000}.jpg",
-                        ImageType = ImageType.Normal,
-                        WidthStart = widthStart,
-                        HeightStart = heightStart,
-                        Width = widthSpan,
-                        Height = heightSpan,
-                    };
-
-                    if (x == widthMod - 1 && y != heightMod - 1)
-                    {
-                        imageSection.Width = widthSpan + widthAdjust;
-                    }
-                    else if (x != widthMod - 1 && y == heightMod - 1)
-                    {
-                        imageSection.Height = heightSpan + heightAdjust;
-                    }
-                    else if (x == widthMod - 1 && y == heightMod - 1)
-                    {
-                        imageSection.Width = widthSpan + widthAdjust;
-                        imageSection.Height = heightSpan + heightAdjust;
-                    }
-
-                    imageSection.Image = bitmap.Clone(new Rectangle(imageSection.WidthStart, imageSection.HeightStart, imageSection.Width, imageSection.Height), PixelFormat.Format24bppRgb);
-                    standardGrid.Add(imageSection);
-                    await UploadImageSectionToBlob(blobContainerClient, imageSection);
-                }
-            }
-
-            return standardGrid;
-        }
-
-        private static async Task<List<ImageSectionDto>> CreateOverlappingImageSections(int widthMod,
-                                                                           int heightMod,
-                                                                           int widthSpan,
-                                                                           int heightSpan,
-                                                                           int imageWidth,
-                                                                           int imageHeight,
-                                                                           Bitmap bitmap,
-                                                                           BlobServiceClient blobServiceClient,
-                                                                           BlobContainerClient blobContainerClient)
-        {
-            var overlappingSections = new List<ImageSectionDto>();
-
-            for (int x = 0; x < widthMod; x++)
-            {
-                for (int y = 0; y < heightMod; y++)
-                {
-                    var widthStart = (x * widthSpan) + (widthSpan / 2);
-                    var heightStart = (y * heightSpan) + (heightSpan / 2);
-
-                    if ((widthStart + widthSpan) < imageWidth && (heightStart + heightSpan) < imageHeight)
-                    {
-                        var imageGrid = new ImageSectionDto
-                        {
-                            Name = $"O_{x:000}-{y:000}.jpg",
-                            WidthStart = widthStart,
-                            HeightStart = heightStart,
-                            ImageType = ImageType.Overlap,
-                            Width = widthSpan,
-                            Height = heightSpan,
-                            Image = bitmap.Clone(new Rectangle(widthStart, heightStart, widthSpan, heightSpan), PixelFormat.Format24bppRgb),
-                        };
-
-                        overlappingSections.Add(imageGrid);
-                        await UploadImageSectionToBlob(blobContainerClient, imageGrid);
-                    }
-                }
-            }
-
-            return overlappingSections;
-        }
-
-        private static List<ImagePortion> ImageToPortions(int width, int height)
+        private static List<ImagePortion> ImageToPortions(decimal width, decimal height)
         {
             var ImagePortions = new List<ImagePortion>();
 
             // Split width and height based on 'static' MaxPixelSpan
-            decimal widthSplitSpan = Math.Ceiling(width / (decimal)MaxPixelSpan);
-            decimal heightSplitSpan = Math.Ceiling(height / (decimal)MaxPixelSpan);
+            decimal widthSplitSpan = Math.Ceiling(width / MaxPixelSpan);
+            decimal heightSplitSpan = Math.Ceiling(height / MaxPixelSpan);
 
             // Reverse process to obtain approximately identically sized images
             decimal widthCalculatedSpan = Math.Ceiling(width / widthSplitSpan);
@@ -315,8 +171,8 @@ namespace AzureFunctions
             int heightCalculatedHalfSpan = (int)Math.Round(heightCalculatedSpan / 2, 0);
 
             // Max cycles in each direction
-            int widthIterations = (int)Math.Round(width / (decimal)widthCalculatedHalfSpan, 0);
-            int heightIterations = (int)Math.Round(height / (decimal)heightCalculatedHalfSpan, 0);
+            int widthIterations = (int)Math.Round(width / widthCalculatedHalfSpan, 0);
+            int heightIterations = (int)Math.Round(height / heightCalculatedHalfSpan, 0);
 
             // Width positions
             for (int i = 0; i < widthIterations; i++)
@@ -340,14 +196,13 @@ namespace AzureFunctions
                             WidthIndex = (int)Math.Floor(i / (decimal)2),
                             WidthIterations = widthIterations,
                             WidthPixel = (int)Math.Floor(i * (decimal)widthCalculatedHalfSpan),
-                            WidthSplit = widthSplitSpan
+                            WidthSplit = widthSplitSpan,
+                            Name = $"F_{Math.Floor(i/(decimal)2):000}-{Math.Floor(j / (decimal)2):000}.jpg",
                         };
-                        imagePortion.Name = $"{imagePortion.Prefix}_{Math.Floor(i/(decimal)2):000}-{Math.Floor(j / (decimal)2):000}.jpg";
                         ImagePortions.Add(imagePortion);
                     }
-                    if (i % 2 == 1 && j % 2 == 1) // Odd iteration values
+                    else // Odd iteration values
                     {
-
                         var imagePortion = new ImagePortion
                         {
                             HeightCalculatedHalfSpan = heightCalculatedHalfSpan,
@@ -362,19 +217,13 @@ namespace AzureFunctions
                             WidthIndex = (int)Math.Floor(i / (decimal)2),
                             WidthIterations = widthIterations,
                             WidthPixel = (int)Math.Floor(i * (decimal)widthCalculatedHalfSpan),
-                            WidthSplit = widthSplitSpan
+                            WidthSplit = widthSplitSpan,
+                            Name = $"O_{Math.Floor(i / (decimal)2):000}-{Math.Floor(j / (decimal)2):000}.jpg",
                         };
-                        imagePortion.Name = $"{imagePortion.Prefix}_{Math.Floor(i / (decimal)2):000}-{Math.Floor(j / (decimal)2):000}.jpg";
                         ImagePortions.Add(imagePortion);
-
                     }
                 }
             }
-
-            ////Image coordinate verification outputs
-            //ImagePortions = ImagePortions.OrderBy(r => r.Name).ToList();
-            //Console.WriteLine("Type\tW Pix\tHPix\tW Ind\tH In\tName");
-            //ImagePortions.ForEach(ip => Console.WriteLine(ip.Prefix + "\t" + ip.WidthPixel + "\t" + ip.HeightPixel + "\t" + ip.WidthIndex + "\t" + ip.HeightIndex + "\t" + ip.Name));
 
             return ImagePortions;
         }
@@ -397,5 +246,7 @@ namespace AzureFunctions
         public int WidthIndex { get; set; }
         public int HeightIndex { get; set; }
         public string Name { get; set; }
+        public decimal AdjustedWith { get => WidthPixel + WidthCalculatedSpan; }
+        public decimal AdjustedHeight { get => HeightPixel + HeightCalculatedSpan; }
     }
 }
